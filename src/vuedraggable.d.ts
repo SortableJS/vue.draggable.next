943e4376911fc5b0370ddf3c3cf1b3bc30d29e5f
Rob Murphy <rob.murphy@wegift.io>
2021-03-09 10:19:28 +0000

    Add ChangeEvent to declared types

--- src/vuedraggable.d.ts
+++ src/vuedraggable.d.ts
@@ -54,6 +54,22 @@ declare module 'vuedraggable' {
     isTrusted: boolean;
   };
 
+  export type ChangeEvent<T> = {
+    added?: {
+      element: T;
+      newIndex: number;
+    };
+    removed?: {
+      element: T;
+      oldIndex: number;
+    };
+    moved?: {
+      element: T;
+      newIndex: number;
+      oldIndex: number;
+    };
+  };
+
   const draggable: ExtendedVue<
     Vue,
     {},
